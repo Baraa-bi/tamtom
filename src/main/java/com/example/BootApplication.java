cb845f765cd7dbc97f955a594e309ac772c71a31
baraa95 <mrbaraa8@gmail.com>
2016-06-03 23:32:01 +0300

    Update BootApplication.java

--- src/main/java/com/example/BootApplication.java
+++ src/main/java/com/example/BootApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.*;
@@ -19,6 +20,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 
 @SpringBootApplication 
+@ConfigurationProperties(prefix = "spring.datasource")
 public class BootApplication {
 	
 
