e00c98891f49a30e2a6ac7b0aace31b19582eece
baraa95 <mrbaraa8@gmail.com>
2016-06-04 00:00:00 +0300

    Update BootApplication.java

--- src/main/java/com/example/BootApplication.java
+++ src/main/java/com/example/BootApplication.java
@@ -1,14 +1,27 @@
 package com.example;
 
+import java.util.Arrays;
+
+import javax.sql.DataSource;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 
 @SpringBootApplication 
 @ConfigurationProperties(prefix = "spring.datasource")
 public class BootApplication {
 	
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
 
     
  public static void main(String[] args)
