504333211ff40b5d3d529d5969d74d275723f007
baraa95 <mrbaraa8@gmail.com>
2016-06-04 00:28:43 +0300

    Update BootApplication.java

--- src/main/java/com/example/BootApplication.java
+++ src/main/java/com/example/BootApplication.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.Primary;
 
 
 @SpringBootApplication 
-@ConfigurationProperties(prefix = "spring.datasource")
 public class BootApplication {
 	
     @Bean
