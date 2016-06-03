819aebaa5b0da29b2bbe1e6590af7608689b075b
baraa95 <mrbaraa8@gmail.com>
2016-06-03 23:25:35 +0300

    Update BootApplication.java

--- src/main/java/com/example/BootApplication.java
+++ src/main/java/com/example/BootApplication.java
@@ -14,22 +14,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.jdbc.datasource;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
 
 
 @SpringBootApplication 
 public class BootApplication {
 	
-	 @Bean
-    @Primary
-    @ConfigurationProperties(prefix = "spring.datasource")
-    public DataSource dataSource() {
-        return DataSourceBuilder.create().build();
-    }
+
     
  public static void main(String[] args)
  {
