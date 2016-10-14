80b75b67ebb03bf0f8ad5133d715041831e47a78
baraa95 <mrbaraa8@gmail.com>
2016-10-14 17:15:47 +0300

    Update upload.java

--- src/main/java/com/example/upload.java
+++ src/main/java/com/example/upload.java
@@ -1,40 +1,32 @@
 package com.example;
 
 
-import org.omg.CORBA.portable.InputStream;
-import org.springframework.core.io.Resource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.io.File;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @RestController
 public class upload {
 
+    HashMap<String , MultipartFile> map = new HashMap<>();
     @RequestMapping("/uploadImage")
     public String upload(@RequestParam("file") MultipartFile file) throws IOException {
-
-        //File file1 = new File("/"+file.getOriginalFilename());
-        //file.transferTo(file1);
-
+        map.put(file.getOriginalFilename(),file);
         return "Your image has been uploaded successfully :D";
     }
-
-//    @RequestMapping
-//    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
-//
-//        Resource file = storageService.loadAsResource(filename);
-//        return ResponseEntity
-//                .ok()
-//                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\""+file.getFilename()+"\"")
-//                .body(file);
-//    }
+    @RequestMapping("/getImage")
+    public MultipartFile get(@RequestParam("name") String name) {
+        
+        return map.get(name);
+                
+    }
 
 }
